d754a70ea7db27c67cdb9577c12f2821f1300f4f
Imransk7768 <imransk7768@gmail.com>
2022-05-09 02:56:39 +0530

    [Imran] Added: Finding Frequency of Words

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Hash Table Programs");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyMapNode<string, int> hash = new MyMapNode<string, int>(5);
+            string words = "to be or not to be";
+            string[] arr = words.Split(' ');
+            LinkedList<string> checkForDuplication = new LinkedList<string>();
+            foreach (string element in arr)
+            {
+                int count = 0;
+                foreach (string match in arr)
+                {
+                    if (element == match)
+                    {
+                        count++;
+                        if (checkForDuplication.Contains(element))
+                        {
+                            break;
+                        }
+                    }
+                }
+                if (!checkForDuplication.Contains(element))
+                {
+                    checkForDuplication.AddLast(element);
+                    hash.Add(element, count);
+                }
+            }
+            hash.Display();
+        }
+    }
+}
